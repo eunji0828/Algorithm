02109e0bb8ca4615905e60ac49086ed460c902fc
eunji0828 <elj0828@hanmail.net>
2023-10-23 23:25:36 +0900

    231023 풀이 완료

---
+++ codingTest_study/src/codingTest_study/baekjoon/loop/별찍기2.java
@@ -0,0 +1,28 @@
+package codingTest_study.baekjoon.loop;
+
+import java.util.Scanner;
+
+public class 별찍기2 {
+
+	public static void main(String[] args) {
+		// 231023
+		// 문제
+		// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+		// 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+
+ 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
