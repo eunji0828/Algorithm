faa44bedd172c0ef4d74617c69249876af743df3
eunji0828 <elj0828@hanmail.net>
2023-11-08 20:54:05 +0900

    Revert "231108 풀이 완료"
    This reverts commit d5d4323aa1548786be1e4335b9d4fc3e0e109920.

--- codingTest_study/codingTest_study/src/codingTest_study/baekjoon/String/문자열.java
+++ codingTest_study/codingTest_study/src/codingTest_study/baekjoon/String/문자열.java
@@ -12,7 +12,7 @@ public class 문자열 {
 		// 231108
 		// 문제
 		// 문자열을 입력으로 주면 문자열의 첫 글자와 마지막 글자를 출력하는 프로그램을 작성하시오.
-		  
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		 
