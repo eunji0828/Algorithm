7f611e301176be856cec6ba34e56b8f2c7fd3cac
eunji0828 <elj0828@hanmail.net>
2023-11-30 15:33:17 +0900

    231120 풀이 완료

---
+++ codingTest_study/codingTest_study/src/codingTest_study/programmer/level0/마지막두원소.java
@@ -0,0 +1,38 @@
+package codingTest_study.programmer.level0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 마지막두원소 {
+	
+	// 231120
+	// 문제
+	// 정수 리스트 num_list가 주어질 때, 
+	// 마지막 원소가 그전 원소보다 크면 마지막 원소에서 
+	// 그전 원소를 뺀 값을 마지막 원소가 그전 원소보다 크지 않다면 
+	// 마지막 원소를 두 배한 값을 추가하여 return하도록 
+	// solution 함수를 완성해주세요.
+	
+	class Solution {
+	    public List solution(int[] num_list) {
+	        List<Integer> answer = new ArrayList<>();
+	        int num = 0;
+	        
+	        for(int i = 0; i < num_list.length; i++){
+	            
+	            answer.add(num_list[i]);
+	            
+	        }
+	        
+	        if(num_list[num_list.length - 1] > num_list[num_list.length - 2]){
+	            answer.add(num_list[num_list.length - 1] - num_list[num_list.length - 2]);
+	        } else {
+	            answer.add(num_list[num_list.length - 1] * 2);
+	        }
+	        
+	        
+	        return answer;
+	    }
+	}
+
+}
