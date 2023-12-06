66fc1568a08d187bb77dbc6fbfd5ffe991332aa3
eunji0828 <elj0828@hanmail.net>
2023-12-06 16:33:42 +0900

    231206 풀이 완료

---
+++ codingTest_study/codingTest_study/src/codingTest_study/programmer/level0/문자열을정수로바꾸기.java
@@ -0,0 +1,16 @@
+package codingTest_study.programmer.level0;
+
+public class 문자열을정수로바꾸기 {
+	
+	// 231206
+	// 문제
+	// 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+	
+	class Solution {
+	    public int solution(String s) {
+	        int answer = Integer.parseInt(s);       
+	        return answer;
+	    }
+	}
+
+}
