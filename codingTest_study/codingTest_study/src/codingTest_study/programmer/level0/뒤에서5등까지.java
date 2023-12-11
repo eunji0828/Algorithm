3f5815cbe9fa704b88e7bdb65b344414bd9e5d02
eunji0828 <elj0828@hanmail.net>
2023-12-11 18:04:47 +0900

    231211 풀이 완료

---
+++ codingTest_study/codingTest_study/src/codingTest_study/programmer/level0/뒤에서5등까지.java
@@ -0,0 +1,27 @@
+package codingTest_study.programmer.level0;
+
+import java.util.Arrays;
+
+public class 뒤에서5등까지 {
+
+	// 231211
+	// 문제
+	// 정수로 이루어진 리스트 num_list가 주어집니다.
+	// num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 
+	// return하도록 solution 함수를 완성해주세요.
+	
+	class Solution {
+	    public int[] solution(int[] num_list) {
+	        int[] answer = new int[5];
+	        
+	        Arrays.sort(num_list);
+	        
+	        for(int i = 0; i < 5; i++){
+	            answer[i] = num_list[i];
+	        }
+	        
+	        return answer;
+	    }
+	}
+	
+}
